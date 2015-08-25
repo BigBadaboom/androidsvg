691356a3d16a5cd2e49d0699f5ec175820ce5a6f
Paul LeBeau <paul.lebeau@gmail.com>
2015-08-26 04:08:54 +1200

    Transfer old Google Code wiki pages to GitHub Pages.

---
+++ doc/reference/lists.js
@@ -0,0 +1,12 @@
+var DATA = [
+      { id:0, label:"com.caverock.androidsvg", link:"com/caverock/androidsvg/package-summary.html", type:"package" },
+      { id:1, label:"com.caverock.androidsvg.PreserveAspectRatio", link:"com/caverock/androidsvg/PreserveAspectRatio.html", type:"class" },
+      { id:2, label:"com.caverock.androidsvg.PreserveAspectRatio.Alignment", link:"com/caverock/androidsvg/PreserveAspectRatio.Alignment.html", type:"class" },
+      { id:3, label:"com.caverock.androidsvg.PreserveAspectRatio.Scale", link:"com/caverock/androidsvg/PreserveAspectRatio.Scale.html", type:"class" },
+      { id:4, label:"com.caverock.androidsvg.SVG", link:"com/caverock/androidsvg/SVG.html", type:"class" },
+      { id:5, label:"com.caverock.androidsvg.SVGExternalFileResolver", link:"com/caverock/androidsvg/SVGExternalFileResolver.html", type:"class" },
+      { id:6, label:"com.caverock.androidsvg.SVGImageView", link:"com/caverock/androidsvg/SVGImageView.html", type:"class" },
+      { id:7, label:"com.caverock.androidsvg.SVGParseException", link:"com/caverock/androidsvg/SVGParseException.html", type:"class" },
+      { id:8, label:"com.caverock.androidsvg.SimpleAssetResolver", link:"com/caverock/androidsvg/SimpleAssetResolver.html", type:"class" }
+
+    ];
